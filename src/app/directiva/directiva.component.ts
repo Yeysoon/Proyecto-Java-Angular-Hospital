2f9087e064487bdf93d61a27225a99b6a907cc18
yeysoon <yeysonaldair13@gmail.com>
2024-03-22 20:08:48 -0600

    Ubicacion de Logo

---
+++ src/app/directiva/directiva.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-directiva',
+  templateUrl: './directiva.component.html',
+})
+export class DirectivaComponent {
+
+  listaCurso: String[] = ['TypeScript','JavaScript','Java SE','C#', 'PHP'];
+  constructor(){  }
+  }
+
---
+++ src/app/imagenes/imagenes.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-imagenes',
+  templateUrl: './imagenes.component.html',
+  styleUrl: './imagenes.component.css'
+})
+export class ImagenesComponent {
+
+}
